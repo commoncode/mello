970a15e11766e46863c46d36c546b2e163b0d735
Tom Coleman <tom@thesnail.org>
2013-02-17 11:09:10 +1000

    Added checkboxes to change lists

--- client/mello.js
+++ client/mello.js
@@ -8,4 +8,23 @@ Template.taskList.helpers({
   tasks: function() {
     return Tasks.find({listId: this._id});
   }
-})
+});
+
+Template.task.helpers({
+  lists: function() {
+    var task = this;
+    return Lists.find().map(function(list) {
+      list.selected = (task.listId === list._id);
+      return list;
+    });
+  },
+  listCheckedAttr: function() {
+    return this.selected && 'checked';
+  }
+});
+
+Template.task.events({
+  'change [name=listId]': function(event, template) {
+    Tasks.update({_id: template.data._id}, {$set: {listId: this._id}});
+  }
+});
