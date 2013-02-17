abad3e2752a752e9dbd1960a55b75f89daa24943
Tom Coleman <tom@thesnail.org>
2013-02-17 10:47:58 +1000

    Wired up collections.

--- client/mello.js
+++ client/mello.js
@@ -1,19 +1,11 @@
-var Lists = [
- {type: 'todo', tasks: [
-   {title: 'Add Collections', description: 'Collections are great'},
-   {title: 'Build out Functionality'}
- ]},
- {type: 'doing', tasks: [
-   {title: 'Learn about helpers'}
- ]},
- {type: 'done', tasks: [
-   {title: 'Create basic HTML and Styles', description: 'Lots of cut and paste blows.'},
-   {title: 'Create meteor app'}
- ]}
-]
-
 Template.taskLists.helpers({
   lists: function() {
-    return Lists;
+    return Lists.find();
+  }
+});
+
+Template.taskList.helpers({
+  tasks: function() {
+    return Tasks.find({listId: this._id});
   }
 })
